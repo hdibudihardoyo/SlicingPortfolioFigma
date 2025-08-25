679c645435da22b713b17ac1cb40ae4603910c8c
hdii <hdibudihardoyo@gmail.com>
2025-08-25 11:51:14 +0700

    style: responsif mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,21 @@ import Avatar from "./Avatar";
 
 const Home = () => {
   return (
-    <section id="home" className="flex flex-col justify-center px-20 py-20 shadow-sm shadow-purple-600/40">
-
+    <section
+      id="home"
+      className="flex flex-col justify-center px-6 md:px-20 py-20 shadow-sm shadow-purple-600/40"
+    >
       {/* Main Content */}
-      <div className="flex flex-row items-center justify-around gap-10">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-10">
+        {/* Home Left */}
+        <HomeLeft />
 
-         {/* Home Left */}
-         <HomeLeft />
+        {/* Avatar */}
+        <Avatar />
 
-         {/* Avatar */}
-         <Avatar />
-        
-         {/* Home Right */}
-         <HomeRight />  
+        {/* Home Right */}
+        <HomeRight />
       </div>
-
-
     </section>
   );
 };
