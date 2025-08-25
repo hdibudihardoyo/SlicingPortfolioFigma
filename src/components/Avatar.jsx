4193002737c1b282413da011203071365945af1f
hdii <hdibudihardoyo@gmail.com>
2025-08-25 11:50:56 +0700

    style: responsif mobile

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 import avatar from "../assets/avatar.svg";
 
 const Avatar = () => {
   return (
-    <div>
+    <div className="flex justify-center">
       {/* Avatar */}
-      <div className="w-[450px] h-[450px] rounded-full overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300">
+      <div className="w-48 h-48 md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] rounded-full overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300">
         <img
-        src={avatar}
-        alt="Avatar"
-        className="w-full h-full object-cover"
+          src={avatar}
+          alt="Avatar"
+          className="w-full h-full object-cover"
         />
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
