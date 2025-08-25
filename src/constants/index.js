0aaba93aa50ed8b8187ae2633cd0bd1ff0c31a70
hdii <hdibudihardoyo@gmail.com>
2025-08-25 12:12:27 +0700

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,22 @@ export const experienceBar = [
   { text: "Puluhan proyek dan", highlight: "Pengalaman" },
   { text: "Desainer Freelance &", highlight: "UI · UX" },
 ];
+
+export const projects = [
+  {
+    title: "Move.it",
+    description:
+      "Aplicação da NLW#04 da Rocketseat. Desenvolvida com React. Plataforma de Pomodoro com exercícios.",
+    tags: ["React Js", "Tailwind CSS"],
+    image: "src/assets/pawsconnect.png",
+    link: "https://moveit.example.com",
+    category: "Dev",
+  },
+];
+
+import { Palette, Workflow, Code2 } from "lucide-react";
+export const filters = [
+    { label: "UI Design", value: "UI", icon: Workflow },
+    { label: "Design", value: "Design", icon: Palette },
+    { label: "Development", value: "Dev", icon: Code2 },
+  ];
